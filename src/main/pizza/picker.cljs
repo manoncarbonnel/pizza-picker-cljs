f280dfe69fa726e1d137a10f6122d2006ce1cd74
Manon Carbonnel <manon.carbonnel@gmail.com>
2026-05-08 20:11:59 +0200

    declare root html component

--- src/main/pizza/picker.cljs
+++ src/main/pizza/picker.cljs
@@ -1,5 +1,20 @@
-(ns pizza.picker)
+(ns pizza.picker
+  (:require
+   [reagent.core :as r]
+   ["react-dom/client" :as rdom]))
 
-(defn main
+(defonce root (r/atom nil))
+
+(defn app []
+  [:h1 "Pizza picker 🍕"])
+
+(defn start
+  {:dev/after-load true}
   []
+  (let [root-el (js/document.getElementById "app")]
+    (reset! root (rdom/createRoot root-el))
+    (.render @root (r/as-element [app]))))
+
+(defn main []
+  (start)
   (js/console.log "Hello pizza lovers 🍕"))
